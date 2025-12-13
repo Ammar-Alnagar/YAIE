e4aeaffbacc6a7a3359ba505e7c0f4d5f8b113e1
Ammar-Alnagar <Ammar-Alnagar@users.noreply.github.com>
2025-12-13 21:28:59 +0000

    deploy: a69445a6303c378b70363d1b155255ffaf2ba587

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fdda5022.js',
+                path_to_root + 'searchindex-b58c91dd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
